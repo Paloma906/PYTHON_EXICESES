b5a1c48cd1b968ed3070b9174d3bfd93a3adb626
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-03-05 16:44:43 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,15 @@
+import customtkinter as ct
+
+ct.set_appearance_mode('dark')
+
+janela = ct.CTk('#434343') 
+janela.geometry('500x300')
+janela.title('Tela de Acesso')
+janela.iconbitmap('icone.ico')
+
+
+
+
+
+
+janela.mainloop()
