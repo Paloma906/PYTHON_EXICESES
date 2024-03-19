1171ba03498b127914066895427aa4e2b3462c23
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-03-19 14:59:27 -0300

    Add files via upload

--- notas.py
+++ notas.py
@@ -1,20 +1,61 @@
-import os 
+import customtkinter as ctk
 
-nome = input('Digite o nome do aluno: ')
-os.system('cls')
-nota1 = float(input('Digite a nota da 1° Unidade: '))
-os.system('cls')
-nota2 = float(input('Digite a nota da 2° Unidade: '))
-os.system('cls')
-nota3 = float(input('Digite a nota da 3° Unidade: '))
-os.system('cls')
+#funções -----------------------------------
 
-media = (nota1+nota2+nota3)/3
+def formula():
+    n1 =float(nota1.get())
+    n2 =float(nota2.get())
+    n3 =float(nota3.get())
+    
+    media = (n1+n2+n3)/3
+    
+    if (media >= 7):
+        resultado.configure(text=f'Sua Média foi {media:.1f}, \n você foi aprovado!')
+    else:
+        resultado.configure(text=f'Sua Média foi {media:.1f}, \n você foi para a recuperação!')
+        
+def limpar():
+    nota1.delete(0,'end')
+    nota2.delete(0,'end')
+    nota3.delete(0,'end')
+    resultado.configure(text='')
+    
+        
 
-if media >=7:
-    print(f'{nome} sua média foi: {media:.1f} você foi aprovado')
-elif media >=5 and media<7:
-    print(f'{nome} sua média foi: {media:.f} você foi para provafinal')
-else:
-   
-    print(f'{nome} sua média foi: {media:.1f} você foi para recuperação')
+
+
+
+#-------------------------------------------
+
+ctk.set_appearance_mode('dark')
+
+janela = ctk.CTk()
+janela.minsize(600,400)
+janela.title('Sistema Escolar V1.0')
+
+ctk.CTkLabel(janela,text='Sistema Escolar versão 1.0',font=('Arial',27,'bold'),text_color='green').pack(pady=5)
+
+nota1 = ctk.CTkEntry(janela,placeholder_text='Digite a nota da 1º Unidade',width=400,height=30)
+nota1.pack(pady=20)
+
+nota2 = ctk.CTkEntry(janela,placeholder_text='Digite a nota da 2º Unidade',width=400,height=30)
+nota2.pack(pady=20)
+
+nota3 = ctk.CTkEntry(janela,placeholder_text='Digite a nota da 3º Unidade',width=400,height=30)
+nota3.pack(pady=20)
+
+botao = ctk.CTkButton(janela,text='Calcular',fg_color='green',text_color='white',width=100,height=30,cursor='star',command=formula)
+botao.pack(pady=10)
+
+botao_limpar = ctk.CTkButton(janela,text='Limpar',fg_color='darkred',text_color='white',width=100,height=30,cursor='star',command=limpar)
+botao_limpar.pack(pady=10)
+
+
+
+resultado = ctk.CTkLabel(janela,text='',font=('Arial',20,'bold'),text_color='white')
+resultado.pack(pady=5)
+
+
+
+
+janela.mainloop()
